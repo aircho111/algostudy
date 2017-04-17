787c8b0485d1e670f9868daa19b3a3145fbd422e
jwook <jwook@LAPTOP-OU4DA9CO>
2017-04-18 00:22:52 +0900

    BufferedReader I/O

--- algoworkshop/src/test/HelloWorld.java
+++ algoworkshop/src/test/HelloWorld.java
@@ -1,8 +1,18 @@
 package test;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class HelloWorld
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		System.out.println("Hello World!");
+		
+		InputStreamReader r = new InputStreamReader(System.in);
+		BufferedReader br = new BufferedReader(r);
+		String userInput = br.readLine();
+		
+		System.out.println("user input :"+userInput);
 	}
 }
